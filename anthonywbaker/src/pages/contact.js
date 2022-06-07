2a084a76692909f9813278966b85499afcebd902
bakera81 <bakera81@icloud.com>
2022-06-06 22:09:14 -0400

    add contact

---
+++ anthonywbaker/src/pages/contact.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+import Layout from  "../components/layout"
+import PageTitle from  "../components/pagetitle"
+import P from "../components/paragraph"
+
+const Contact = () => (
+  <Layout>
+    <PageTitle>Contact</PageTitle>
+    <P><b>Electronic-mail:</b> anthonywbaker1[at]gmail.com</P>
+    <P><b>LinkedIn:</b> <a href="https://www.linkedin.com/in/awbaker1/" target="_blank" rel="noreferrer">linkedin.com/in/awbaker1/</a></P>
+    <P><b>Github:</b>  <a href="https://github.com/bakera81" target="_blank" rel="noreferrer">github.com/bakera81</a></P>
+    <P><b>Twitter:</b> <a href="https://twitter.com/AddieBundren81" target="_blank" rel="noreferrer">twitter.com/AddieBundren81</a></P>
+  </Layout>
+)
+
+export default Contact;
