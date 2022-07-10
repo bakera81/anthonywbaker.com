030c60bf63f28da3fef3443c3395c60f3c86ecbc
bakera81 <bakera81@icloud.com>
2022-07-10 18:11:44 -0400

    style footer image

--- next/anthonydotcomputer/components/footer.js
+++ next/anthonydotcomputer/components/footer.js
@@ -1,6 +1,7 @@
 import styles from './footer.module.css'
 import P from './paragraph'
 import A from './anchor'
+import Image from 'next/image';
 
 export default function Footer({ home }) {
     return(
@@ -8,6 +9,15 @@ export default function Footer({ home }) {
             {/* <Img fluid={randomImg.node.childImageSharp.fluid}
            imgStyle={{objectFit: `cover`, objectPosition: `50% ${randomCropPct}%`, height: `250px`}}
            style={{height: `250px`}} /> */}
+           <div className={styles.footerImgWrapper}>
+           <Image 
+                src="/images/renaissance/2560px-Sandro_Botticelli_-_La_nascita_di_Venere_-_Google_Art_Project_-_edited.jpg"
+                height={250}
+                layout="fill"
+                objectFit="cover"
+                className={styles.footerImg}
+           />
+           </div>
            {/* TODO: Fix this */}
            {home ? (
                 <></>
