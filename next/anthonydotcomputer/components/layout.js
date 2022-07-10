4f10214c278710a1b335b8abcdcad58e1cdd0868
bakera81 <bakera81@icloud.com>
2022-07-09 20:44:25 -0400

    add logic to top level layout component

--- next/anthonydotcomputer/components/layout.js
+++ next/anthonydotcomputer/components/layout.js
@@ -1,10 +1,42 @@
 import Navbar from './navbar'
+import Footer from './footer'
 
-export default function Layout({ children }) {
+const renderFooter = props => {
+  if (props.hideFooter) {
+    return
+  }
+  return (
+    <Footer hideNav={props.hideNav} />
+  )
+}
+
+const renderNav = props => {
+  if (props.hideName) {
+    return
+  }
+  return <NavBar />
+}
+
+export default function Layout({ children, home }) {
   return (
     <>
-      <Navbar />
-      {children}
+    {home ? (
+      <section className="section">a
+        <div className="container">
+          {children}
+        </div>
+      </section>
+      ) : (
+        <>
+          <Navbar />
+          <section className="section">
+            <div className="container">
+              {children}
+            </div>
+          </section>
+          <Footer />
+        </>
+    )}
     </>
   );
 }
