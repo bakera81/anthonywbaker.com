7a4a2b891599dbf66dbb2c7a999e1043d994af30
bakera81 <bakera81@icloud.com>
2022-07-22 20:03:56 -0400

    remove next Image from pagetitle

--- next/anthonydotcomputer/components/pagetitle.js
+++ next/anthonydotcomputer/components/pagetitle.js
@@ -31,13 +31,20 @@ export default function ({ children }) {
       <div className="container">
         <div className="level">
           <div className={`level-right has-text-right is-hidden-mobile ${style.level}`}>
-          {/*TODO: FLip this to fluid and make it fill the height title is-1  */}
-            <Image 
-                src="/images/renaissance/2560px-Sandro_Botticelli_-_La_nascita_di_Venere_-_Google_Art_Project_-_edited.jpg"
-                height={85}
-                layout="fill"
-                objectFit="cover"
+            <img 
+              className={style.img} 
+              src="/images/renaissance/2560px-Sandro_Botticelli_-_La_nascita_di_Venere_-_Google_Art_Project_-_edited.jpg" 
+              alt="Another Renaissance painting"
             />
+                {/*TODO: FLip this to fluid and make it fill the height title is-1  */}
+                {/* TODO: Figure out how to style this image. Maybe use fixed and get the dimensions */}
+                  {/* <Image 
+                      src="/images/renaissance/2560px-Sandro_Botticelli_-_La_nascita_di_Venere_-_Google_Art_Project_-_edited.jpg"
+                      // height={85}
+                      // className="level-item"
+                      layout="fill"
+                      // objectFit="contain"
+                  /> */}
           </div>
           <div className="level-right">
             <h1 className={`title is-1 ${style.pageTitle}`}>{children}</h1>
