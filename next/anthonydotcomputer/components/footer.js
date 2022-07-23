e4960bcf0e61cea255220a3da6b4ea367f1f358a
bakera81 <bakera81@icloud.com>
2022-07-22 20:35:44 -0400

    add randomization to footer

--- next/anthonydotcomputer/components/footer.js
+++ next/anthonydotcomputer/components/footer.js
@@ -2,19 +2,25 @@ import styles from './footer.module.css'
 import P from './paragraph'
 import A from './anchor'
 import Image from 'next/image';
-import { css } from '@emotion/react'
+import allImages from '../helpers/images'
 
 export default function Footer({ home }) {
     //TODO: Fix this
-    const allImgStyles = [styles.footerImg0, styles.footerImg10, styles.footerImg20, styles.footerImg30, styles.footerImg40,
-         styles.footerImg50, styles.footerImg60, styles.footerImg70, styles.footerImg80, styles.footerImg90, styles.footerImg100];
+    const allImgStyles = [
+        styles.footerImg0, styles.footerImg10, styles.footerImg20, styles.footerImg30, 
+        styles.footerImg40, styles.footerImg50, styles.footerImg60, styles.footerImg70, 
+        styles.footerImg80, styles.footerImg90, styles.footerImg100,
+    ];
+    
     const randomImgStyle = allImgStyles[Math.floor(Math.random()*allImgStyles.length)];
+    const randomImg = allImages[Math.floor(Math.random() * allImages.length)]
 
     return(
         <footer className={`footer ${home ? styles.footerNoNav : styles.footerNav}`}>
            <div className={styles.footerImgWrapper}>
             <Image 
-                src="/images/renaissance/2560px-Sandro_Botticelli_-_La_nascita_di_Venere_-_Google_Art_Project_-_edited.jpg"
+                // src="/images/renaissance/2560px-Sandro_Botticelli_-_La_nascita_di_Venere_-_Google_Art_Project_-_edited.jpg"
+                src={randomImg}
                 height={250}
                 layout="fill"
                 objectFit="cover"
