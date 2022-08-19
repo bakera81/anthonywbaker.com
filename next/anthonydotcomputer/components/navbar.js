dcd6830aa09ca43113e743d5aa1d105c1527bf6a
bakera81 <bakera81@icloud.com>
2022-08-19 13:54:06 -0400

    fix: navbar styling

--- next/anthonydotcomputer/components/navbar.js
+++ next/anthonydotcomputer/components/navbar.js
@@ -1,14 +1,14 @@
-import { css } from '@emotion/react'
-import styles from './navbar.module.css'
 import A from './anchor'
 import Back from './back'
 
-export default function Navbar () {
-  return(
+import styles from './navbar.module.css'
+
+export default function Navbar() {
+  return (
     <nav className="navbar" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
-        <A href="/" className={styles.logo}>ANTHONY W. BAKER</A>
-        <Back className={styles.logo} />
+        <A href="/" className={`navbar-item ${styles.logo}`}>Anthony W. Baker</A>
+        <Back className={`navbar-item ${styles.logo}`} />
       </div>
     </nav>
   );
