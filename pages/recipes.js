6ad641f0df0a17c685ac0fd4b8050e4f7819606d
bakera81 <bakera81@icloud.com>
2022-10-10 17:27:02 -0400

    WIP: recipes from Notion

--- pages/recipes.js
+++ pages/recipes.js
@@ -5,11 +5,13 @@ import RecipeList from '../components/recipeList'
 
 import styles from './recipes.module.css'
 
-import { getRecipesData } from '../utils/recipes'
+import { getRecipeBlocks } from '../utils/recipes'
+
+// import { getRecipesData } from '../utils/recipes'
 
 
 export async function getStaticProps() {
-    const recipesData = getRecipesData();
+    const recipesData = await getRecipeBlocks();
     return {
       props: {
         recipesData,
@@ -17,31 +19,42 @@ export async function getStaticProps() {
     };
 }
 
-export default function Recipes ({ recipesData }) {
-
+export default function Recipes({ recipesData }) { 
   return (
-    <Layout title="Recipes">
-      <PageTitle>Recipes</PageTitle>
-        <div className="section">
-            <div className="columns">
-                {recipesData.slice(0,2).map((category) => (
-                    <div className="column is-6">
-                        <h2 className={styles.recipeSection}>{category.category}</h2>
-                        <RecipeList recipeData={category.data} />
-                    </div>
-                ))}
-            </div>
-        </div>
-        <div className="section">
-            <div className="columns">
-                {recipesData.slice(2,4).map((category) => (
-                    <div className="column is-6">
-                        <h2 className={styles.recipeSection}>{category.category}</h2>
-                        <RecipeList recipeData={category.data} />
-                    </div>
-                ))}
-            </div>
-        </div>                    
-    </Layout>
+    <>
+      {console.log(recipesData)}
+      {recipesData.map((recipe) => (
+        <p>{recipe.child_page.title}</p>
+      ))}
+    </>
   )
 }
+
+// export default function Recipes ({ recipesData }) {
+
+//   return (
+//     <Layout title="Recipes">
+//       <PageTitle>Recipes</PageTitle>
+//         <div className="section">
+//             <div className="columns">
+//                 {recipesData.slice(0,2).map((category) => (
+//                     <div className="column is-6">
+//                         <h2 className={styles.recipeSection}>{category.category}</h2>
+//                         <RecipeList recipeData={category.data} />
+//                     </div>
+//                 ))}
+//             </div>
+//         </div>
+//         <div className="section">
+//             <div className="columns">
+//                 {recipesData.slice(2,4).map((category) => (
+//                     <div className="column is-6">
+//                         <h2 className={styles.recipeSection}>{category.category}</h2>
+//                         <RecipeList recipeData={category.data} />
+//                     </div>
+//                 ))}
+//             </div>
+//         </div>                    
+//     </Layout>
+//   )
+// }
--- utils/recipes.js
+++ utils/recipes.js
@@ -1,51 +1,116 @@
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
+// const { Client } = require("@notionhq/client")
+import { Client, collectPaginatedAPI } from '@notionhq/client'
 
-var GithubSlugger = require('github-slugger')
-var slugger = new GithubSlugger()
 
-const recipesDirectory = path.join(process.cwd(), 'data', 'recipes');
+// Initializing a client
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+})
 
-export function getRecipesData() {
+export async function getRecipeBlocks() { 
+    const blockId = '9c55f35c31644f748c7a11c5081ef810';
+    const allBlocks = await notion.blocks.children.list({
+        block_id: blockId,
+        page_size: 50,
+    })
+    // console.log(allBlocks.results)
 
-    const dirNames = fs.readdirSync(recipesDirectory);
-    const allRecipesData = dirNames.map((dirName) => {
-        // Read all files in subdirectory
-        const dirPath =  path.join(recipesDirectory, dirName)
-        const fileNames = fs.readdirSync(dirPath);
+    return allBlocks.results;
 
-        // const recipesCategoryData = [];
-        var recipesCategoryData = fileNames.reduce((recipesCategoryData, fileName) => {
+    // const blockId = '9c55f35c31644f748c7a11c5081ef810';
+    // const blocks = await collectPaginatedAPI(notion.blocks.children.list, {
+    //     block_id: blockId,
+    //   }).then(blocks => {
+    //     return blocks.map((block) => {
+    //         return {
+    //             ...block
+    //         }
+    //     })
+    //   })
+    // console.log(blocks)
+    
+    // const pageId = 'c373d6b64c0340d1b5a0e622711870d6';
+    // const response = await notion.pages.retrieve({ page_id: pageId });
 
-            if (path.extname(fileName) === '.md') {
-                // console.log('processing...')
-                // Read markdown file as string
-                const fullPath = path.join(recipesDirectory, dirName, fileName)
-                const fileContents = fs.readFileSync(fullPath, 'utf8');    
 
-                // Use gray-matter to parse the post metadata section
-                const matterResult = matter(fileContents);
-                recipesCategoryData.push({
-                    fileName: fileName,
-                    path: fullPath,
-                    slug: slugger.slug(matterResult.data.title),
-                    content: matterResult.content,
-                    ...matterResult.data,
-                });
-            }
-            return recipesCategoryData
-        }, []);
+    // const blockId = '9c55f35c31644f748c7a11c5081ef810';
+    // const allBlocks = await notion.blocks.children.list({
+    //     block_id: blockId,
+    //     page_size: 50,
+    // }).then(blocks => {
+    //     return blocks
+    // });
+    // console.log(allBlocks);
+    // return allBlocks;
+
+    // (async () => {
+    //     const blockId = '9c55f35c31644f748c7a11c5081ef810';
+    //     const response = await notion.blocks.children.list({
+    //       block_id: blockId,
+    //       page_size: 50,
+    //     });
+    //     console.log(response);
+    //   })();
+    // (async () => {
+    //     const pageId = 'c373d6b64c0340d1b5a0e622711870d6';
+    //     const response = await notion.pages.retrieve({ page_id: pageId });
+    //     console.log("RESPONSE:")
+    //     console.log(response);
+    //     return response;
+    //   })();
+
+};
+
+
+
+// import fs from 'fs';
+// import path from 'path';
+// import matter from 'gray-matter';
+
+// var GithubSlugger = require('github-slugger')
+// var slugger = new GithubSlugger()
+
+// const recipesDirectory = path.join(process.cwd(), 'data', 'recipes');
+
+// export function getRecipesData() {
+
+//     const dirNames = fs.readdirSync(recipesDirectory);
+//     const allRecipesData = dirNames.map((dirName) => {
+//         // Read all files in subdirectory
+//         const dirPath =  path.join(recipesDirectory, dirName)
+//         const fileNames = fs.readdirSync(dirPath);
+
+//         // const recipesCategoryData = [];
+//         var recipesCategoryData = fileNames.reduce((recipesCategoryData, fileName) => {
+
+//             if (path.extname(fileName) === '.md') {
+//                 // console.log('processing...')
+//                 // Read markdown file as string
+//                 const fullPath = path.join(recipesDirectory, dirName, fileName)
+//                 const fileContents = fs.readFileSync(fullPath, 'utf8');    
+
+//                 // Use gray-matter to parse the post metadata section
+//                 const matterResult = matter(fileContents);
+//                 recipesCategoryData.push({
+//                     fileName: fileName,
+//                     path: fullPath,
+//                     slug: slugger.slug(matterResult.data.title),
+//                     content: matterResult.content,
+//                     ...matterResult.data,
+//                 });
+//             }
+//             return recipesCategoryData
+//         }, []);
 
         
-        return {
-            category: dirName,
-            data: recipesCategoryData,
-        }
+//         return {
+//             category: dirName,
+//             data: recipesCategoryData,
+//         }
         
-    });
+//     });
 
-    // console.log('ALL RECIPES DATA')
-    // console.log(allRecipesData)
-    return allRecipesData;
-}
+//     // console.log('ALL RECIPES DATA')
+//     // console.log(allRecipesData)
+//     return allRecipesData;
+// }
