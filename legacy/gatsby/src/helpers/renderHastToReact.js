9168b5f2a5a703f30e970c75713cfe04bcb16769
bakera81 <bakera81@icloud.com>
2023-01-17 21:25:31 -0500

    remove legacy Gatsby site

--- legacy/gatsby/src/helpers/renderHastToReact.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import hastToHyperscript from "hast-to-hyperscript";
-
-const renderHastToReact = node => {
-    return hastToHyperscript(React.createElement, node);
-}
-
-export default renderHastToReact
