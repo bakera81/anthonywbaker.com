d28fa60e5171ea447573ccd37dd67658edbf229f
bakera81 <bakera81@icloud.com>
2023-04-03 20:52:03 -0400

    add a filter for Airtable for DJ Assistant

--- utils/djAssistant.js
+++ utils/djAssistant.js
@@ -4,11 +4,15 @@ var base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base('appAnc9ynU
 export async function getAirtableData() {
 
     // const allRecords = await base.select().all()
-    const allRecords = await base('Songs for People').select({view: 'Prod'}).all().then(records => {
-        // records array will contain every record in Main View.
-        return records.map((record) => {
-            return {...record.fields}
-        });
+    const allRecords = await base('Songs for People').select({
+            view: 'Prod', 
+            filterByFormula: `{Status} = 'Live'`
+        }).all()
+        .then(records => {
+            // records array will contain every record in Main View.
+            return records.map((record) => {
+                return {...record.fields}
+            });
     }).catch(err => {
         // Handle error.
         console.error(err); 
