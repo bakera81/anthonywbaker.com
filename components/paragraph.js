301fe04a9f89a980cc02dc85eb7178b1b18f4955
bakera81 <bakera81@icloud.com>
2023-06-14 22:09:50 -0400

    New project: things on the street

--- components/paragraph.js
+++ components/paragraph.js
@@ -1,5 +1,6 @@
 import styles from './paragraph.module.css'
 
+// TODO: Have this pass through className
 export default function P ({ children, style }) {
     return (
         <p className={styles.paragraph} style={style}>{children}</p>
