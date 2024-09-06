9e27c0e1f1fb6df4506e8a868601e8a03b607a18
bakera81 <bakera81@icloud.com>
2024-09-05 22:23:02 -0400

    Try reuploading lambs room

--- pages/wedding/index.js
+++ pages/wedding/index.js
@@ -8,7 +8,7 @@ import PageTitle from '../../components/wedding/pagetitle'
 
 import styles from './index.module.css'
 
-import receptionImg from '../../public/images/wedding/lambs_room.jpg'
+import receptionImg from '../../public/images/wedding/lambs_room_2.jpg'
 import threeWitchesImg from '../../public/images/wedding/three_witches.jpg'
 import bat1Img from '../../public/images/wedding/bat1.jpg'
 import eye1Img from '../../public/images/wedding/eye1.jpg'
