5e6a18b56d2321b3fd845d8ce713986865b989b5
bakera81 <bakera81@icloud.com>
2025-03-08 08:20:52 -0600

    Update styling to have a separate coming soon section

--- components/recipeList.js
+++ components/recipeList.js
@@ -5,19 +5,42 @@ import Link from 'next/link'
 import styles from './recipeList.module.css'
 
 export default function RecipeList({ recipeData }) {
+    const comingSoonRecipes = recipeData.filter(recipe => !recipe.slug);
+    const availableRecipes = recipeData.filter(recipe => recipe.slug);
+
     return (
-        <ul>
-            {recipeData.map((recipe) => (
-                <li>
-                    {recipe.slug ? (
-                        <A href={`/recipes/${recipe.slug}`}> 
-                            <P leftAlign>{recipe.title}</P>
-                        </A>
-                    ) : (
-                        <P leftAlign>{recipe.title} [coming soon]</P>
-                    )}
-                </li>
-            ))}
-        </ul>
+        <>
+            <ul>
+                {availableRecipes.map((recipe) => (
+                    <li>
+                        {recipe.slug ? (
+                            <A href={`/recipes/${recipe.slug}`}> 
+                                <P leftAlign>{recipe.title}</P>
+                            </A>
+                        ) : (
+                            <P leftAlign>{recipe.title} [coming soon]</P>
+                        )}
+                    </li>
+                ))}
+            </ul>
+            {comingSoonRecipes.length > 0 && (
+                <>
+                    <P style={{fontWeight: 'bold'}}>COMING SOON:</P>
+                    <ul>
+                        {comingSoonRecipes.map((recipe) => (
+                            <li>
+                                {recipe.slug ? (
+                                    <A href={`/recipes/${recipe.slug}`}> 
+                                        <P leftAlign>{recipe.title}</P>
+                                    </A>
+                                ) : (
+                                    <P className={styles.comingSoon} leftAlign>{recipe.title}</P>
+                                )}
+                            </li>
+                        ))}
+                    </ul>
+                </>
+            )}
+        </>
     )
 }
