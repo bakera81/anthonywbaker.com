dd418047803cb12fa9bba852edcf4198a86e5f4e
bakera81 <bakera81@icloud.com>
2025-04-08 20:32:40 -0400

    Remove twitter

--- pages/contact.js
+++ pages/contact.js
@@ -9,7 +9,6 @@ export default function Contact() {
             <P><b>Electronic-mail:</b> anthonywbaker1[at]gmail.com</P>
             <P><b>LinkedIn:</b> <a href="https://www.linkedin.com/in/awbaker1/" target="_blank" rel="noopener">linkedin.com/in/awbaker1/</a></P>
             <P><b>Github:</b>  <a href="https://github.com/bakera81" target="_blank" rel="noopener">github.com/bakera81</a></P>
-            <P><b>Twitter:</b> <a href="https://twitter.com/AddieBundren81" target="_blank" rel="noopener">twitter.com/AddieBundren81</a></P>
         </Layout>
     )
 }
